0e42901b784efb71f946d5d9dcf0febcea5fb87f
MAC <nwokikesamson@gmail.com>
2020-03-06 14:29:21 +0100

    updated vue config file for lower vendor bundle size

--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,16 @@ module.exports = {
       headless: true,
       onlyProduction: true
     }
+  },
+  configureWebpack:{
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      }
+    }
   }
 }
