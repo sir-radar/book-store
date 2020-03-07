720b5b673de641c9375108f055f6bca4ac4067a5
MAC <nwokikesamson@gmail.com>
2020-03-07 01:40:41 +0100

    minor changes to route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+    component: Home
   },
   {
     path: '/about',
